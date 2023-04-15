8d38956c67a04562e53da4ba769574a5abb016cf
Sushil <skshinde2028@gmail.com>
2023-04-15 21:46:57 +0530

    [Sushil] adding flip coin program

---
+++ C#Day5BasicCoreProgram/FlipCoin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day5BasicCoreProgram
+{
+    internal class FlipCoin
+    {
+        public static void FlipACoin()
+        {
+            double headPercentage = 0;
+            double tailPercentage = 0;
+            int heads = 0;
+            int tails = 0;
+
+            for (int i = 1; i < 100; i++)
+            {
+                Random random = new Random();
+                int coin = random.Next(1, 3);
+                if (coin == 1)
+                {
+                    heads += 1;
+                }
+                else if (coin == 2)
+                {
+                    tails += 1;
+                }
+                else
+                {
+                    Console.WriteLine("out ArgumentOutOfRangeException ifff");
+                }
+
+                // Console.ReadLine();
+            }
+            headPercentage = (heads / 10.0);
+            tailPercentage = 100 - headPercentage;
+            Console.WriteLine("headPercentage: " + headPercentage);
+            Console.WriteLine("Head: " + heads);
+            Console.WriteLine("tailPercentage: " + tailPercentage);
+            Console.WriteLine("Tails: " + tails);
+
+        }
+    }
+}
--- C#Day5BasicCoreProgram/Program.cs
+++ C#Day5BasicCoreProgram/Program.cs
@@ -11,6 +11,21 @@ namespace C_Day5BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the c# basic core problems");
+            Console.WriteLine("Choose option for program from below options");
+            Console.WriteLine("1.FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());  
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Coin is flipped 100 times ");
+                    FlipCoin.FlipACoin();
+                    break;
+                default:
+                    Console.WriteLine("Please choose a valid option");
+                    break;
+
+            }
         }
     }
 }
